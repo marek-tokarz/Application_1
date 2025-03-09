5f06af6907ca3eadab364e05b99080e03cf8d202
marek-tokarz <marek.tokarz14@gmail.com>
2025-03-09 10:02:27 +0100

    Added a new line to print out a message in Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    Console.WriteLine("Test of commiting changes");
+    Console.WriteLine("Test of commiting changes from a remote GitHub repository");
+    Console.WriteLine("Test of commiting changes from a local repository in Visual Studio");
 }
 
 app.UseHttpsRedirection();
